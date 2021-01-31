41f245cf44e638d130573b1b1335586b4813227c
mrExit <73747014+Xepuyc@users.noreply.github.com>
2021-01-31 12:49:36 +0700

    1
    1

---
+++ scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] GameObject _knife;
+    bool _isTarget = false;
+    
+    private void OnMouseDown()
+    {
+        
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,20f));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (!_isTarget&&collision.collider.CompareTag("Target"))
+        {
+            _isTarget = true;
+            this.gameObject.transform.SetParent(collision.transform);
+            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            
+        }else if(collision.collider.CompareTag("Knife")&&!_isTarget)
+        {
+           
+            gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-10,10), -5));
+            StartCoroutine(Rotation());
+            
+            
+           
+        }
+        
+      
+    }
+    private IEnumerator Rotation()
+    {
+        gameObject.transform.Rotate(new Vector3(0, 0, 10));
+        yield return null;
+        yield return StartCoroutine(Rotation());
+    }
+}
---
+++ scripts/TargetControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetControl : MonoBehaviour
+{
+    [SerializeField] float _rotationZ;
+    [SerializeField] GameObject _knife;
+
+
+    void Update()
+    {
+        Rotation();
+    }
+    private void Start()
+    {
+        Instantiate(_knife);
+    }
+
+
+    private void Rotation()
+    {
+        this.transform.Rotate(0,0,_rotationZ);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(_knife);
+    }
+}
